f7e63501c536371fef90597e3e8b3f2f3025bf1f
coolmandave1 <git config --global user.name coolmandave1>
2018-09-08 09:43:56 +0800

    v2

--- app/src/main/java/com/example/davefernandez/fernandezdavelab2/MainActivity.java
+++ app/src/main/java/com/example/davefernandez/fernandezdavelab2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.davefernandez.fernandezdavelab1;
+package com.example.davefernandez.fernandezdavelab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
